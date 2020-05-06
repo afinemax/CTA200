d230804085ee569a2a9065eded8cc32dfe14405a
afinemax <afinemax@gmail.com>
2020-05-05 22:51:32 -0400

    BONUS: increased the avg to be 1-7, and a joke

--- main.c
+++ main.c
@@ -3,10 +3,13 @@
 
 
 int main() {
-    double arr[] = {1.0, 2.0, 3.0, 4.0};
+    double arr[] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0};
 
-    double result = average(4, arr);
-
-    printf("The average of 1, 2, 3 and 4 is: %.4f\n", result);
+    double result = average(7, arr);
+    
+    /* increased the average to be 1-7 and added a joke to the return */
+	
+    printf("On an odd note, this is my GPA\n");
+    printf("The average of the numbers of 1-7 is: %.4f\n", result);
     return 0;    
 }
